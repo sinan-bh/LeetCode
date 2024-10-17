70450a1dcf831016df993caf33e43587b87f80a5
sinan <sinanbh9605@gmail.com>
2024-10-17 18:59:34 +0530

    second commit

--- JS/leetCode.js
+++ JS/leetCode.js
@@ -19,3 +19,28 @@ var numberOfPairs = function(nums1, nums2, k) {
     return sum.length
 };
 
+// 3151. Special Array I
+var isArraySpecial = function(nums) {
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] % 2 === nums[i + 1] % 2) return false
+    }
+    return true
+};
+
+//3142. Check if Grid Satisfies Conditions
+var satisfiesConditions = function (grid) {
+    const g = grid.length;
+    const l = grid[0].length;
+    for (let i = 0; i < grid.length; i++){
+        for ( let j = 0; j < grid[0].length; j++){
+            if (i < g - 1 && grid[i][j] !== grid[i + 1][j]) return false
+            if (j < l - 1 && grid[i][j] === grid[i][j + 1]) return false
+        }
+    }
+    return true
+};
+
+// 3131. Find the Integer Added to Array I
+var addedInteger = function(nums1, nums2) {
+    return Math.max(...nums2) - Math.max(...nums1)
+};
