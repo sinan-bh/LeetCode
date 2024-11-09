c78129df5ee3e3599123d4ed21e0f79656d7ec5b
sinan <sinanbh9605@gmail.com>
2024-11-09 12:30:32 +0530

    09/11/2024

--- JS/medium.js
+++ JS/medium.js
@@ -27,3 +27,36 @@ var compressedString = function (word) {
   }
   return arr.join("");
 };
+
+
+//945. Minimum Increment to Make Array Unique
+var minIncrementForUnique = function (nums) {
+  let sorted = nums.sort((a,b)=> a - b)
+  let res = 0;
+  let inc = 0;
+  for (let i = 1; i < sorted.length; i++) {
+      if (sorted[i] <= sorted[i - 1]) {
+        inc = sorted[i - 1] - sorted[i] + 1
+        sorted[i] += inc;
+        res += inc
+    }
+  }
+  return res;
+}
+
+//633. Sum of Square Numbers
+var judgeSquareSum = function (c) {
+  var r = Math.floor(Math.sqrt(c));  
+  var l = 0;
+  while (l <= r) {
+      var sum = l ** 2 + r ** 2;
+      if (sum == c) {
+          return true;
+      } else if (sum < c) {
+          l++;
+      } else if (sum > c) {
+          r--;
+      }
+  }
+  return false;
+};
