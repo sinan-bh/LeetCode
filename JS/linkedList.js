6816017b153615daa0e07a68a9632e60b3833968
sinan <sinanbh9605@gmail.com>
2024-12-16 10:18:11 +0530

    16/12/2024

--- JS/linkedList.js
+++ JS/linkedList.js
@@ -59,3 +59,37 @@ const root = new TreeNode(
 );
 
 // console.log(inorderTraversal(root));
+
+//21. Merge Two Sorted Lists
+class ListNode {
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+
+    var mergeTwoLists = function (list1, list2) {
+      let value = new ListNode(-1);
+      let temp = value;
+      console.log(temp);
+
+      while (list1 !== null && list2 !== null) {
+        if (list1.val <= list2.val) {
+          temp.next = list1;
+          list1 = list1.next;
+        } else {
+          temp.next = list2;
+          list2 = list2.next;
+        }
+        current = current.next;
+      }
+
+      if (list1 !== null) temp.next = list1;
+      if (list2 !== null) temp.next = list2;
+
+      return val.next;
+    };
+    const head = new ListNode(1, new ListNode(2, new ListNode(4)));
+    const head2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+    console.log(mergeTwoLists(head, head2));
+    // console.log(distanceBetweenBusStops([7,6,3,0,3],0,4));
+  }
+}
--- JS/text.js
+++ JS/text.js
@@ -1,21 +1,33 @@
-var isBalanced = function (num) {
-  let a = 0;
-  let b = 0;
-  for (let i = 0; i < num.length; i++) {
-    if (i % 2 !== 0) {
-      a += Number(num[i]);
-    } else {
-      b += Number(num[i]);
-    }
-  }
-  console.log(a, b);
+class ListNode {
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
 
-  if (a === b) {
-    return true;
-  }
+    var mergeTwoLists = function (list1, list2) {
+      let value = new ListNode(-1);
+      let temp = value;
+      console.log(temp);
 
-  return false;
-};
-// const head = new ListNode(1, new ListNode(1, new ListNode(2)));
-console.log(isBalanced("24123"));
+      while (list1 !== null && list2 !== null) {
+        if (list1.val <= list2.val) {
+          temp.next = list1;
+          list1 = list1.next;
+        } else {
+          temp.next = list2;
+          list2 = list2.next;
+        }
+        current = current.next;
+      }
+
+      if (list1 !== null) temp.next = list1;
+      if (list2 !== null) temp.next = list2;
+
+      return val.next;
+    };
+    const head = new ListNode(1, new ListNode(2, new ListNode(4)));
+    const head2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+    console.log(mergeTwoLists(head, head2));
+    // console.log(distanceBetweenBusStops([7,6,3,0,3],0,4));
+  }
+}
 // console.log(distanceBetweenBusStops([7,6,3,0,3],0,4));
