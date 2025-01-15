e1fd1a536435b3e8ee1700d5b40ba244006a161a
sinan <sinanbh9605@gmail.com>
2025-01-15 10:30:39 +0530

    15/01/2025

--- JS/easy.js
+++ JS/easy.js
@@ -372,3 +372,21 @@ var splitWordsBySeparator = function (words, separator) {
 
   return -1
 };
+
+// 169. Majority Element
+var majorityElement = function (nums) {
+  let n = 0;
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+      if (count === 0) {
+          n = nums[i];
+      }
+      if (n === nums[i]) {
+          count++;
+      } else {
+          count--;
+      }
+  }
+  return n;
+};
